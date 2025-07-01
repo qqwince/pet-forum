7878c1da7d62d85659cf95572bcc91f095d33c5f
qqwince <kasssaim291@gmail.com>
2025-07-01 18:42:03 +0500

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,31 @@
+import React, {Suspense, useContext, useState} from 'react';
+import "./styles/index.scss"
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import {useTranslation} from "react-i18next";
+
+
+
+const App = () => {
+    const {theme} = useTheme();
+
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter/>
+                </div>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
