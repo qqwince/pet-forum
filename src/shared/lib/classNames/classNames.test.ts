2e4c2a589c3ac0ef56ae1576ca71d0d2072e02d5
qqwince <kasssaim291@gmail.com>
2025-07-02 19:13:40 +0500

    add babel, jest, first test :D

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,46 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('1st param only', () => {
+        expect(classNames('something')).toBe('something');
+    });
+
+    test('3rd param: additional classes', () => {
+        const expected = 'something class1 class2';
+        expect(
+            classNames(
+                'something',
+                {},
+                ['class1', 'class2'],
+            ),
+        )
+            .toBe(expected);
+    });
+
+    test('with mods: all true', () => {
+        const expected = 'something class1 class2 hovered scrollable';
+        expect(classNames(
+            'something',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+
+    test('with mods: one false', () => {
+        const expected = 'something class1 class2 hovered';
+        expect(classNames(
+            'something',
+            { hovered: true, scrollable: false },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+
+    test('with mods: one undefined', () => {
+        const expected = 'something class1 class2 hovered';
+        expect(classNames(
+            'something',
+            { hovered: true, scrollable: undefined },
+            ['class1', 'class2'],
+        )).toBe(expected);
+    });
+});
