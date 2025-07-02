749a97f490db34ea00b08d2fcd306cd60374a133
qqwince <kasssaim291@gmail.com>
2025-07-02 18:06:32 +0500

    fix .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,22 @@
-const i18next = require('i18next');
-
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
-        'airbnb',
         'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
+        'airbnb',
         'plugin:i18next/recommended',
     ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
     plugins: [
         'react',
         '@typescript-eslint',
@@ -33,19 +26,23 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+        'react/jsx-filename-extension': [
+            2,
+            { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
         'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
         'no-shadow': 'off',
         'import/extensions': 'off',
-        'import/no-extraneous-dependencies': 0,
+        'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'no-unused-vars': 'warn',
-        '@typescript-eslint/no-unused-vars': 'error',
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
