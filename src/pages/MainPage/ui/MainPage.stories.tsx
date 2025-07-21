4890382bd11e2d2a0a6abd7c138acd3d9baf4814
qqwince <kasssaim291@gmail.com>
2025-07-21 12:08:37 +0500

    fix MainPage.stories.tsx

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+// eslint-disable-next-line storybook/no-renderer-packages
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import MainPage from './MainPage';
+import { MainPage } from 'pages/MainPage';
 
 export default {
     title: 'pages/MainPage',
