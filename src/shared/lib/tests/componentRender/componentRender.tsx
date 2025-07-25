48cbb2e7fa47bb25965684caaad35aee38fee086
qqwince <kasssaim291@gmail.com>
2025-07-25 16:01:50 +0500

    fix tests

---
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+export interface componentRenderOption {
+    route?: string;
+}
+
+export default function componentRender(component: ReactNode, options: componentRenderOption = {}) {
+    const {
+        route = '/',
+    } = options;
+    return render(
+        <MemoryRouter initialEntries={[route]}>
+            <I18nextProvider i18n={i18nForTests}>
+                {component}
+            </I18nextProvider>
+        </MemoryRouter>,
+    );
+}
