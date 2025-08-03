aa7cc4717a70b19d056c2b6509a262fc34400e67
qqwince <kasssaim291@gmail.com>
2025-08-03 13:31:50 +0500

    add new input, login, form ui and stories for them

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// eslint-disable-next-line storybook/no-renderer-packages
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import 'app/styles/index.scss';
+
+import { LoginForm } from './LoginForm';
+
+export default {
+    title: 'features/LoginForm',
+    component: LoginForm,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// eslint-disable-next-line storybook/no-renderer-packages
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import 'app/styles/index.scss';
+
+import { Input } from './Input';
+
+export default {
+    title: 'shared/Input',
+    component: Input,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    placeholder: 'type text',
+    value: '123321',
+};
